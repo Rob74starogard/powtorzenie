b89b2db2111608737f75a71481e488afa4369965
Rob74starogard <92817159+Rob74starogard@users.noreply.github.com>
2024-03-13 08:20:04 +0100

    small fix in main

--- src/main/java/pl/akademiaspecjalistowit/powtorzeniematerialu/PowtorzenieMaterialuApplication.java
+++ src/main/java/pl/akademiaspecjalistowit/powtorzeniematerialu/PowtorzenieMaterialuApplication.java
@@ -6,7 +6,7 @@ public class PowtorzenieMaterialuApplication {
 
     public static void main(String[] args) {
         MeetingApp meetingApp = new MeetingApp();
-        meetingApp.fill();
+        meetingApp.run();
     }
 
 }
